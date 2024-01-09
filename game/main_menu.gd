caddcfedb22276db83c30045d121edca6280eba5
Bratah123 <brandonnguyen3301@gmail.com>
2024-01-08 18:49:40 -0800

    FEAT: Add start button textured

--- game/main_menu.gd
+++ game/main_menu.gd
@@ -12,3 +12,7 @@ func _process(delta):
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://game/game.tscn")
+
+
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://game/game.tscn")
