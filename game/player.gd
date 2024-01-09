a5a1ebde7d5bafa8bd6ea27a73d4859f30929136
Angie <angietea101@gmail.com>
2024-01-08 19:22:40 -0800

    UPDATE: Player movement logic

--- game/player.gd
+++ game/player.gd
@@ -2,12 +2,10 @@ extends CharacterBody2D
 
 @export var move_speed : float = 200
 
-func _physics_process(_delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+func handle_input():
+	var move_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = move_direction * move_speed
 	
-	velocity = input_direction * move_speed
-	
+func _physics_process(delta):
+	handle_input()
 	move_and_slide()
