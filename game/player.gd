052906ae4de3ce51942ed2a31ce1d6d40ae2280b
Bratah123 <brandonnguyen3301@gmail.com>
2024-01-11 00:06:51 -0800

    FIX: player grabbing bugs
    Bug that caused the player to be able to grab items from across the map

--- game/player.gd
+++ game/player.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @export var move_speed : float = 200
 @onready var animations = $AnimationPlayer
 var is_holding = false
-var is_in_area = false
 const grabbable_object = preload("res://game/grabbables/grabbable.gd")
 var grabbable_in_area
 var root_scene
@@ -12,18 +11,26 @@ var held_grabbable
 func _ready():
 	root_scene = get_parent()
 
-func handle_input():
-	var move_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = move_direction * move_speed
-	if Input.is_action_just_pressed("click") and is_in_area and not is_holding:
-		grabbable_in_area.reparent($PickableArea)
-		held_grabbable = grabbable_in_area
+func _handle_pick_up():
+	if Input.is_action_just_pressed("click") and not is_holding:
+		# Check if the player is pickable area is hovering over any items
+		var items_in_area = $PickableArea.get_overlapping_bodies()
+		if len(items_in_area) == 0:
+			return
+		# Pick up the first thing in the list
+		held_grabbable = items_in_area[0]
+		held_grabbable.reparent($PickableArea)
 		is_holding = true
 		_play_pickup_sound()
 	elif Input.is_action_just_pressed("click") and is_holding:
 		held_grabbable.reparent(root_scene)
 		held_grabbable = null
 		is_holding = false
+
+func handle_input():
+	var move_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = move_direction * move_speed
+	_handle_pick_up()
 		
 func update_animation():
 	if velocity.length() == 0:
@@ -37,18 +44,6 @@ func _physics_process(delta):
 	update_animation()
 	# Rotate towards mouse
 	look_at(get_global_mouse_position())
-
-func _on_pickable_area_body_entered(body):
-	if body is grabbable_object and not is_in_area and not is_holding:
-		grabbable_in_area = body
-		is_in_area = true
-
-func _on_pickable_area_body_exited(body):
-	if body != held_grabbable:
-		return
-	if body is grabbable_object:
-		grabbable_in_area = null
-		is_in_area = false
 
 func _play_pickup_sound():
 	$PickUpSound.play(.6)
