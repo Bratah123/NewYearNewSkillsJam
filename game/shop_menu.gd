a3a809ff5ffaffb85cb6a39201e078ecdb13a604
Fynn <Tung02nguyen10@gmail.com>
2024-01-13 01:18:15 -0800

    FEAT: implement back button

--- game/shop_menu.gd
+++ game/shop_menu.gd
@@ -1,15 +1,12 @@
 extends CanvasLayer
 
 @onready var game = $"../"
+@onready var shop_menu = $ShopMenu
 var seed_bag = preload("res://game/grabbables/carrot_seed.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-func _on_button_pressed():
-	pass # Replace with function body.
-
 
 func _on_carrot_pressed():
 	var instance = seed_bag.instantiate()
@@ -35,6 +32,5 @@ func _on_corn_pressed():
 	var instance = seed_bag.instantiate()
 	add_child(instance)
 
-
 func _on_texture_button_pressed():
-	pass # Replace with function body.
+	shop_menu.visible = false
