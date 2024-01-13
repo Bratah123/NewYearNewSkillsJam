7f95c5182f33957f8bfafcc366bb198374b62307
Angie <angietea101@gmail.com>
2024-01-13 06:16:35 -0800

    TWEAK: Change seed bag spawn point next to NPC

--- game/shop_menu.gd
+++ game/shop_menu.gd
@@ -10,23 +10,31 @@ func _ready():
 
 func _on_carrot_pressed():
 	var instance = seed_bag.instantiate()
+	_set_position(instance)
 	add_child(instance)
 
 func _on_radish_pressed():
 	var instance = seed_bag.instantiate()
+	_set_position(instance)
 	add_child(instance)
 
 func _on_tomato_pressed():
 	var instance = seed_bag.instantiate()
+	_set_position(instance)
 	add_child(instance)
 
 func _on_potato_pressed():
 	var instance = seed_bag.instantiate()
+	_set_position(instance)
 	add_child(instance)
 
 func _on_corn_pressed():
 	var instance = seed_bag.instantiate()
+	_set_position(instance)
 	add_child(instance)
 
 func _on_texture_button_pressed():
 	shop_menu.visible = false
+
+func _set_position(seed_bag):
+	seed_bag.position = Vector2(90, 610)
