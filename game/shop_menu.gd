a51eb0109cf37c1c617046bec11286e7059666d7
Fynn <Tung02nguyen10@gmail.com>
2024-01-14 00:38:44 -0800

    FEAT: correct seed bags spawn

--- game/shop_menu.gd
+++ game/shop_menu.gd
@@ -2,34 +2,38 @@ extends CanvasLayer
 
 @onready var game = $"../"
 @onready var shop_menu = $ShopMenu
-var seed_bag = preload("res://game/grabbables/carrot_seed.tscn")
+var carrot_bag = preload("res://game/grabbables/carrot_seed.tscn")
+var radish_bag = preload("res://game/grabbables/radish_seed.tscn")
+var tomato_bag = preload("res://game/grabbables/tomato_seed.tscn")
+var potato_bag = preload("res://game/grabbables/potato_seed.tscn")
+var corn_bag = preload("res://game/grabbables/corn_seed.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _on_carrot_pressed():
-	var instance = seed_bag.instantiate()
+	var instance = carrot_bag.instantiate()
 	_set_position(instance)
 	add_child(instance)
 
 func _on_radish_pressed():
-	var instance = seed_bag.instantiate()
+	var instance = radish_bag.instantiate()
 	_set_position(instance)
 	add_child(instance)
 
 func _on_tomato_pressed():
-	var instance = seed_bag.instantiate()
+	var instance = tomato_bag.instantiate()
 	_set_position(instance)
 	add_child(instance)
 
 func _on_potato_pressed():
-	var instance = seed_bag.instantiate()
+	var instance = potato_bag.instantiate()
 	_set_position(instance)
 	add_child(instance)
 
 func _on_corn_pressed():
-	var instance = seed_bag.instantiate()
+	var instance = corn_bag.instantiate()
 	_set_position(instance)
 	add_child(instance)
 
